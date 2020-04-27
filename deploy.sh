98fff300f48f84755da4ffae2913154a54c96be6
liushuang <188007803@qq.com>
2020-04-27 13:00:24 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:188007803/188007803.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:188007803/notebook.git master:gh-pages
+# git push -f git@github.com:188007803/notebook.git master:gh-pages
+git push -f ${access_token}@github.com:188007803/notebook.git master:gh-pages
 
 cd -
