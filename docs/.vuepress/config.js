5f2d526a04acd6b4cb7d11a262df79a07afc31f9
liushuang <188007803@qq.com>
2020-04-30 18:56:33 +0800

    f

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,28 +21,35 @@ module.exports = {
         ],
         nav: [
             {
+                text: '吗哪',
+                link: '/gospel/manna'
+            },
+            {
                 text: '前端',
-                items: [
-                    {
-                        text: 'VueElementAdmin调整',
-                        link: '/frontend/vue-element-admin'
-                    }
-                ]
+                link: '/frontend/vue-element-admin',
+                // items: [
+                //     {
+                //         text: 'VueElementAdmin调整',
+                //         link: '/frontend/vue-element-admin'
+                //     }
+                // ]
             },
             {
                 text: '后端',
-                items: [
-                    { text: '直播服务搭建笔记', link: '/backend/live-stream' },
-                    { text: 'RabbitMQ消息队列', link: '/backend/rabbit-mq' },
-                    { text: 'Docker笔记',    link: '/backend/docker' },
-                ]
+                link: '/backend/live-stream',
+                // items: [
+                //     { text: '直播服务搭建笔记', link: '/backend/live-stream' },
+                //     { text: 'RabbitMQ消息队列', link: '/backend/rabbit-mq' },
+                //     { text: 'Docker笔记',    link: '/backend/docker' },
+                // ]
             },
             {
                 text: '算法',
-                items: [
-                    { text: '常用算法',  link: '/algorithm/common' },
-                    { text: '二叉树相关', link: '/algorithm/tree' },
-                ]
+                link: '/algorithm/common',
+                // items: [
+                //     { text: '常用算法',  link: '/algorithm/common' },
+                //     { text: '二叉树相关', link: '/algorithm/tree' },
+                // ]
             }
         ],
         sidebar: [
