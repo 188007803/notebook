61e972a189e21ba7077aa2a02ae9babdbd8b192b
liushuang <188007803@qq.com>
2020-04-30 12:20:05 +0800

    f

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -66,6 +66,7 @@ module.exports = {
                 children: [
                     '/algorithm/common',
                     '/algorithm/tree',
+                    '/algorithm/heap',
                 ]
             },
             {
